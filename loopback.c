bed411d3ffe0a87c39bb36a1070ea9e2f085c76f
Konrad Beckmann <konrad.beckmann@gmail.com>
2018-12-21 01:41:48 +0100

    loopie

--- loopback.c
+++ loopback.c
@@ -13,13 +13,18 @@ void udp_callback(int fd, short event, void *arg)
 {
 	int val;
 
-	char *buf = malloc(1024 * 128);
+	static uint16_t buf[4096];
 	val = read(fd, buf, 4096);
 	printf("GOT EVENT %d!\n", val);
+	for (int x = 0; x < val/2; x++) {
+		printf("%04x ", buf[x]);
+		// if (x % 16 == 0)
+			// printf("\n");
+	}
+	printf("\n");
 
 	if (val > 0)
 		write(fd, buf, val);
-	free(buf);
 }
 
 int main(int argc, char **argv)
@@ -47,22 +52,35 @@ int main(int argc, char **argv)
 	ev = event_new(base, in, EV_READ | EV_PERSIST, &udp_callback, NULL);
 	event_add(ev, NULL);
 
+	printf("Before write\n");
+	write(in, buf, 2048);
+	write(in, buf, 2048);
+	printf("After write\n");
+
+#if 0
     struct timeval tv;
 	uint32_t last_sec = 0;
 	uint64_t bytes = 0;
+	int first = 1;
 	while (1) {
+		first = 0;
 		write(in, buf, 2048);
 		bytes += 2048;
 
 	    gettimeofday(&tv, NULL);
-		if (tv.tv_sec != last_sec) {
+		if (!first && tv.tv_sec != last_sec) {
 			last_sec = tv.tv_sec;
 			printf("%llu bytes/s\n", bytes);
 			printf("%llu Mbytes/s\n", bytes / 1024 / 1024);
+#	if 0
+	char *buf = malloc(1024 * 128);
+	int val = read(in, buf, 1);
+	printf("GOT EVENT %d!\n", val);
+#	endif
 			bytes = 0;
 		}
 	}
-
+#endif
 	event_base_dispatch(base);
 	event_base_free(base);
 

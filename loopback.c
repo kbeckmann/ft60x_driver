b582913d5ab728b5afe79cc3ee6d1bce29ec869d
Konrad Beckmann <konrad.beckmann@gmail.com>
2018-12-25 23:21:39 +0100

    Horrible mess

--- loopback.c
+++ loopback.c
@@ -8,23 +8,47 @@
 #include <stdlib.h>
 #include <time.h>
 
+uint32_t last_sec = 0;
+uint64_t bytes_out = 0;
+uint64_t bytes_in = 0;
 
 void udp_callback(int fd, short event, void *arg)
 {
 	int val;
 
-	static uint16_t buf[4096];
-	val = read(fd, buf, 4096);
+	static uint16_t buf[40960];
+	printf("GOT EVENT...\n");
+	printf("read(\n");
+	val = read(fd, buf, 40960);
+	printf(")\n");
+	bytes_in += val;
+
+	// enable for SPAM
 	printf("GOT EVENT %d!\n", val);
+#if 0
 	for (int x = 0; x < val/2; x++) {
 		printf("%04x ", buf[x]);
 		// if (x % 16 == 0)
 			// printf("\n");
 	}
 	printf("\n");
+#endif
 
+	printf("write(\n");
 	if (val > 0)
-		write(fd, buf, val);
+		bytes_out += write(fd, buf, val);
+		// bytes_out += write(fd, buf, val);
+	printf(")\n");
+
+    struct timeval tv;
+	gettimeofday(&tv, NULL);
+	if (tv.tv_sec != last_sec) {
+		last_sec = tv.tv_sec;
+		printf("IN: %llu B/s, OUT: %llu B/s\n", bytes_in, bytes_out);
+		printf("IN: %llu Mbytes/s, %llu Mbytes/s\n", bytes_in / 1024 / 1024, bytes_out / 1024 / 1024);
+		bytes_in = 0;
+		bytes_out = 0;
+	}
 }
 
 int main(int argc, char **argv)
@@ -44,7 +68,8 @@ int main(int argc, char **argv)
 	buf = malloc(128 * 1024);
 
 	for (i = 0; i < 128 * 1024; i++) {
-		buf[i] = 'A' + (i % 64);
+		// buf[i] = 'A' + (i % 64);
+		buf[i] = '\xAA';
 	}
 
 	in = open(argv[1], O_RDWR | O_CLOEXEC);
@@ -58,9 +83,13 @@ int main(int argc, char **argv)
 	event_add(ev, NULL);
 
 	printf("Before write\n");
-	write(in, buf, 2048);
-	write(in, buf, 2048);
+	write(in, buf, 2);
+	// write(in, buf, 2048);
+	// write(in, buf, 2048);
+	// write(in, buf, 2048);
+	// write(in, buf, 2048);
 	printf("After write\n");
+	udp_callback(in, 0, 0); //fake
 
 #if 0
     struct timeval tv;
